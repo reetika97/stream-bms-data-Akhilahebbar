9ae565cd8911530dff9471695910a3b34734993f
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-05-28 12:14:04 +0530

    Add files via upload

---
+++ BatteryDataStreamer.Test/BatteryDataStreamerTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BatteryDataStreamer.Test
+{
+
+    [TestClass]
+    public class BatteryDataStreamerTest
+    {
+        [TestMethod]
+        public void GetBatteryInputParameters_ValidReturnValue()
+        {
+            IBatteryInputGenerator batterygenerator = new BatteryInputGenerator();
+
+            BmsBattery result = batterygenerator.GetBatteryInputParameters();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void GetBatteryTemperature_ValidDataWithinRange()
+        {
+            IBatteryInputGenerator batterygenerator = new BatteryInputGenerator();
+            BmsBattery batteryParameter = new BmsBattery();
+            batteryParameter.Temperature = batterygenerator.GetBatteryTemperature();
+            Assert.IsTrue(batteryParameter.Temperature > Constants.MinTemperature && batteryParameter.Temperature < Constants.MaxTemperature);
+        }
+        [TestMethod]
+        public void GetBatteryChargeRate_ValidDataWithinRange()
+        {
+            IBatteryInputGenerator batterygenerator = new BatteryInputGenerator();
+            BmsBattery batteryParameter = new BmsBattery();
+            batteryParameter.ChargeRate = batterygenerator.GetBatteryChargeRate();
+            Assert.IsTrue(batteryParameter.ChargeRate > Constants.MinChargeRate && batteryParameter.ChargeRate < Constants.MaxChargeRate);
+        }
+        [TestMethod]
+        public void GenerateRandomNumber_ValidNumber()
+        {
+            IBatteryInputGenerator batterygenerator = new BatteryInputGenerator();
+            double resultValue = batterygenerator.GenerateRandomNumber(Constants.MinChargeRate, Constants.MaxChargeRate);
+            Assert.IsNotNull(resultValue);
+        }
+    }
+}
+
